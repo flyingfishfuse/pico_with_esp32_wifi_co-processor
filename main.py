c1119d6e7afc88ddfdc9e01da208df3a1624cba8
flyingfishfusealt <46322912+flyingfishfuse@users.noreply.github.com>
2022-11-13 09:02:02 -0500

    Update main.py

--- main.py
+++ main.py
@@ -59,9 +59,98 @@ from adafruit_io.adafruit_io import IO_MQTT
 #                - internet -> phone
 #                - WLAN -> phone
 #                - display_esp32 (planned)
+class Config:
+    '''
+    Config class for holding all your secrets and endpoints
+    '''
+    def __init__(self):
+        '''
+        waaat
+        '''
+        self.ssid           = "Untrusted Network"
+        self.password       = 'Whatapassword1!'
+        self.aio_username   = 'your_adafruit_io_username_'
+        self.aio_key        = 'your_big_huge_super_long_aio_key_'
+        self.broker         = 'io.adafruit.com',
+        self.port           = '1883'
+
+class Sensors:
+    '''
+    Possibly unnecessary metaclass for representing the sensors on the esp32
+    '''
+    
+class MQTTManager:
+    def __init__(self,config:Config):
+        '''
+        This class is the handler for all mqtt activities
+
+        It holds the callbacks and the init as well as dial out, dial in
+        '''
+        # do nothing, user must call init themself
+        self.aio_username   = config.aio_username
+        self.aio_passkey    = config.aio_key
+        self.broker         = config.broker
+
+
+    def init_mqtt(self):
+        '''
+        Dials out to adafruit broker
+        '''
+        # Set your Adafruit IO Username and Key in secrets.py
+        # (visit io.adafruit.com if you need to create an account,
+        # or if you need your Adafruit IO key.)
+        aio_username = self.aio_username
+        aio_key = self.aio_passkey
+
+        # original adafruit code stub
+        #print("Connecting to %s" % secrets["ssid"])
+        #wifi.radio.connect(secrets["ssid"], secrets["password"])
+        #print("Connected to %s!" % secrets["ssid"])
+        
+        ### Feeds ###
+
+        # Setup a feed named 'photocell' for publishing to a feed
+        #photocell_feed = self.aio_username + "/feeds/photocell"
+
+        # Setup a feed named 'onoff' for subscribing to changes
+        #onoff_feed = self.aio_username + "/feeds/onoff"
+
+    def create_feeds(self,new_feeds:dict):
+        '''
+        Creates endpoints for publishing and subscribing
+        input:
+            list_of_feeds = [new_endpoint_name_1','new_endpoint_name_2']
+        
+        This will be added as class member via setattr()
+        output:
+            self.new_endpoint_name_1 = "aio_username/feeds/new_endpoint_name_1
+        '''
+        for each in new_feeds:
+            setattr(self,each ,f"{self.aio_username}/feeds/{each}")
+
+    # Define callback methods which are called when events occur
+    # pylint: disable=unused-argument, redefined-outer-name
+    def connected(client, userdata, flags, rc):
+        # This function will be called when the client is connected
+        # successfully to the broker.
+        print("Connected to Adafruit IO! Listening for topic changes on %s" % onoff_feed)
+        # Subscribe to all changes on the onoff_feed.
+        client.subscribe(onoff_feed)
+
+
+    def disconnected(client, userdata, rc):
+        # This method is called when the client is disconnected
+        print("Disconnected from Adafruit IO!")
+
+
+    def message(client, topic, message):
+        # This method is called when a topic the client is subscribed to
+        # has a new message.
+        print("New message on topic {0}: {1}".format(topic, message))
+
 
 class Pico:
-    def __init__(self):
+    def __init__(self,config:Config):
         '''
         UNFINISHED
         metaclass to keep pico in scope
@@ -400,8 +489,17 @@ if __name__ == "__main__":
 # initialization step 1
 # setup devices
 
+    #create config for setting auth and endpoints
+    config = Config()
+    # if your WLAN SSID differs from my test network setup, you might want to change your
+    # ssid and password here
+    # same with adafruit creds
+    config.ssid = "Other Network"
+    # but that was just an example, this is going to run on my network so I am going to change the name back!
+    config.ssid = "Untrusted Network"
     # create wrapper/reference for main board
-    pico = Pico()
+    # with configuration
+    pico = Pico(config)
 
     # create new wifi manager class
     esp_device = Esp32WifiDevice(pico)
