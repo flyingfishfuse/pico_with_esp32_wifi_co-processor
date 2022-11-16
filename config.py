4cca53c3e41c616afa40a2d365106888f5b31c82
flyingfishfusealt <46322912+flyingfishfuse@users.noreply.github.com>
2022-11-16 10:47:38 -0500

    narf

--- config.py
+++ config.py
@@ -1,3 +1,5 @@
+import board
+
 class Config:
     '''
     Config class for holding all your secrets and endpoints
@@ -6,6 +8,7 @@ class Config:
         '''
         waaat
         '''
+        self.debug          =  True
         self.ssid           = "Untrusted Network"
         self.password       = 'Whatapassword1!'
         # the name is important, read the code in:
@@ -15,6 +18,18 @@ class Config:
         # do not change names, these items are for specific activities
         self.mqtt_username   = 'your_adafruit_io_username_'
         self.mqtt_key        = 'your_big_huge_super_long_aio_key_'
-        self.broker         = 'io.adafruit.com',
-        self.port           = '1883'
+        self.broker          = 'io.adafruit.com',
+        self.port            = '1883'
+
+        #oled settings
+
+        #pinout
+        # was thinking of moving this here but I might just
+        # put "pinout" in its own file or class
+        #self.oled_i2c_sda    = board.GP18 
+        #self.oled_i2c_scl    = board.GP19 
+        self.HEIGHT          = 128
+        self.WIDTH           = 64
+        self.BORDER          = 5
+        self.oled_i2c_addr   = 0x3c
         
