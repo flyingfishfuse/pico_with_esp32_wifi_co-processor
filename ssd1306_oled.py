afbc99f3b7ac115c2be564a8785a1c4c7ac04922
flyingfishfusealt <46322912+flyingfishfuse@users.noreply.github.com>
2022-12-01 11:22:55 -0500

    I pulled a freaking wire out O_o

--- ssd1306_oled.py
+++ ssd1306_oled.py
@@ -1,7 +1,7 @@
 ################################
 # Internal
 ################################
-import board
+#import board
 import busio
 
 ################################
@@ -39,12 +39,12 @@ class SSD1306:
         # height and the border size, which we will explain a little further 
         # below. If your display is something different than these numbers, 
         # change them to the correct setting.
-        self.WIDTH = 128
-        self.WIDTH = 64
-        self.BORDER = 5
+        #self.WIDTH = 128
+        #self.WIDTH = 64
+        #self.BORDER = 5
 
         self.WIDTH = config.WIDTH
-        self.WIDTH = config.HEIGHT
+        self.HEIGHT = config.HEIGHT
         self.BORDER = config.BORDER
 
 
@@ -61,7 +61,10 @@ class SSD1306:
         the display pins are not automatically released and this 
         makes them available for use again.
         '''
-        displayio.release_displays()
+        try:
+            displayio.release_displays()
+        except:
+            print("[-] Failed to release display")            
     
     def set_display(self,reset_pin=False):
         '''
@@ -77,53 +80,66 @@ class SSD1306:
         function, it finds the SPI module and initializes using the default SPI 
         parameters. We also set the display bus to I2CDisplay which makes use of the I2C bus.
         '''
-        if reset_pin == False:
-            self.display_bus = displayio.I2CDisplay(self.i2c, device_address=self.device_address)
-        elif reset_pin == True:
-            self.display_bus = displayio.I2CDisplay(self.i2c, 
-                                                    device_address=self.device_address, 
-                                                    reset=self.reset_pin)
-        
+        try:
+            if reset_pin == False:
+                print("[+] Setting setting display, no reset pin")
+                self.display_bus = displayio.I2CDisplay(self.i2c, device_address=self.device_address)
+            elif reset_pin == True:
+                self.display_bus = displayio.I2CDisplay(self.i2c, 
+                                                        device_address=self.device_address, 
+                                                        reset=self.reset_pin)
+        except:
+            print("[-] Failed to set display")
+
     def show_terminal(self):
         '''
         initialize the driver with a width of the WIDTH variable and a height
         of the HEIGHT variable. If we stopped at this point and ran the code,
         we would have a terminal that we could type at and have the screen update.
         '''
-        self.display = adafruit_displayio_ssd1306.SSD1306(self.display_bus, 
-                                                          WIDTH=self.WIDTH,
-                                                          WIDTH=self.WIDTH
-                                                        )
-
+        try:
+            self.display = adafruit_displayio_ssd1306.SSD1306(self.display_bus, 
+                                                              WIDTH=self.WIDTH,
+                                                              WIDTH=self.WIDTH
+                                                            )
+        except:
+            print("[-]")        
 
 ###############################################################################
 # pinout
 ###############################################################################   
 
-    def setpins(self):
+    def setpins(self, i2s_sda,i2c_scl,reset_pin):
         '''
         pinout for module to pico via i2c
+        python has a limitation preventing this from being easy to assign 
+        pins in a pretty way
         '''
-        #---------------- SDA ----------------*
-        # ss1306 OLED           = SDA 
-        # pico                  = GP18, <I2C1 SDA> , physical pin 24
-        self.i2c_sda            = board.GP18
-        #self.i2c_sda = config.oled_i2c_sda
-        #
-        #----------------SCL -----------------*
-        # ss1306 OLED           = SCL 
-        # pico                  = GP19, <I2C1 SCL> , physical pin 25
-        self.i2c_scl            = board.GP19
-        #self.i2c_scl = config.oled_i2c_scl
-        #--------------Reset ----------------*
-        # ss1306 OLED           = RES
-        # pico                  = GP09 I guess? Adafruit, fix your tutorials
-        self.reset_pin = False # board.D9
+        try:
+            #---------------- SDA ----------------*
+            # ss1306 OLED           = SDA 
+            # pico                  = GP18, <I2C1 SDA> , physical pin 24
+            self.i2c_sda            = i2s_sda#board.GP18
+            #self.i2c_sda = config.oled_i2c_sda
+            #
+            #----------------SCL -----------------*
+            # ss1306 OLED           = SCL 
+            # pico                  = GP19, <I2C1 SCL> , physical pin 25
+            self.i2c_scl            = i2c_scl#board.GP19
+            #self.i2c_scl = config.oled_i2c_scl
+            #--------------Reset ----------------*
+            # ss1306 OLED           = RES
+            # pico                  = GP09 I guess? Adafruit, fix your tutorials
+            self.reset_pin          = reset_pin # board.D9
+        except:
+            print("[-]")
 
     def init_I2C(self):
         '''
         initializes I2C communications between pico and OLED module
         '''
-        self.i2c_bus = busio.I2C(scl=self.i2c_scl, sda=self.i2c_sda)
-
+        try:
+            self.i2c_bus = busio.I2C(scl=self.i2c_scl, sda=self.i2c_sda)
+        except:
+            print("[-] Failed to activate I2C bus on SSD1306")
  
